49ca2b28e3eb9d80844dc4dcbcb05095be8d4c9c
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-07-31 02:05:25 +0800

    when pressing D form is detecting - key pressed captured

---
+++ keyboard_press_external_detected/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input; 
+using System.Threading; 
+
+
+
+namespace keyboard_press_external_detected
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+      
+        bool isRunning = true;
+        private object key;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Thread TH = new Thread(Keyboardd);
+            TH.SetApartmentState(ApartmentState.STA);
+            CheckForIllegalCrossThreadCalls = false;
+            TH.Start(); 
+        }
+
+
+
+
+        /**
+        * To use Keyboard class you need to add references 
+        * PresentationCore and System.Xml.Ling, I am not sure if included with System.Xml 
+         */
+        void Keyboardd()
+        {
+            while(isRunning)
+            {
+                Thread.Sleep(40);
+
+                if ((Keyboard.GetKeyStates(Key.D) & KeyStates.Down) > 0)
+                {
+
+                    label1.Text = "D Pressed";
+
+                    Console.WriteLine(label1.Text); 
+
+                } 
+                else
+                {
+                    label1.Text = "D Not Pressed";
+                    Console.WriteLine(label1.Text);
+                } 
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            isRunning = false;
+        }
+    }
+}
